57126ef04d7164a1e48d571c57b26f478ef2c0bc
Misaka450 <40377@yzk.com>
2025-08-14 13:31:28 +0800

    修复部署错误：补全注释语法

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -72,7 +72,7 @@ function AdminSidebar({ isOpen, onClose }) {
         />
       )}
       
-      {/* 侧边栏 - 恢复必要的响应式动画 */
+      {/* 侧边栏 - 恢复必要的响应式动画 */}
       <div className={`fixed left-0 top-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
         isOpen ? 'translate-x-0' : '-translate-x-full'
       } lg:translate-x-0`}>
